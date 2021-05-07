0cabc40c6c41298537814fc49c9daf1dc18b08d8
Laura Messa Travin <laura.travin@gmail.com>
2021-05-07 21:03:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -123,6 +123,19 @@ sortBy: function(array, callback){
 
 //FLATTEN--------------------------------------------------------------------------------
 
+flatten: function(collection, shallow, newArr=[]) {
+   if (!Array.isArray(collection)) return newArr.push(collection)
+   if (shallow) {
+     for (let val of collection)
+       Array.isArray(val) ? this.unpack(newArr, val) : newArr.push(val)
+   } else {
+     for (let val of collection) {
+       this.flatten(val, false, newArr)
+     }
+   }
+   return newArr
+ },
+
 
 
 
